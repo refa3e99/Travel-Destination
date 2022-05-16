665a2de4766cdbd109c3c36d9068b8da6488cfcd
refa3e99 <refa3e99@icloud.com>
2022-05-16 18:52:28 +0300

    styled

--- travel/src/components/Navbar/Navbar.js
+++ travel/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "../Navbar/Navbar.css";
 function Navbar(props){
 
     return (
--- travel/src/components/header/Header.js
+++ travel/src/components/header/Header.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import '../header/Header.css';
+import Navbar from "../Navbar/Navbar";
 function Header(props){
 
     return (
 
         <div className="header">
             <h1>welcome</h1>
+            <Navbar/>
         </div>
     
     );
